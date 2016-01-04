f77d4e4b110eeb592d9e67c0d058d7065323ed73
yakhinvadim <yakhin.v@gmail.com>
2016-01-04 18:54:22 +0300

    add event listeners for pips and change pips' names

--- app/blocks/javascript-skill/javascript-skill.js
+++ app/blocks/javascript-skill/javascript-skill.js
@@ -10,3 +10,19 @@ noUiSlider.create(javascriptSkill, {
 		max: 10
 	}
 });
+
+document.querySelector('.javascript-skill__pip_lvl_beginner').addEventListener('click', function () {
+	javascriptSkill.noUiSlider.set(0);
+});
+
+document.querySelector('.javascript-skill__pip_lvl_junior').addEventListener('click', function () {
+	javascriptSkill.noUiSlider.set(2);
+});
+
+document.querySelector('.javascript-skill__pip_lvl_middle').addEventListener('click', function () {
+	javascriptSkill.noUiSlider.set(5);
+});
+
+document.querySelector('.javascript-skill__pip_lvl_senior').addEventListener('click', function () {
+	javascriptSkill.noUiSlider.set(10);
+});
